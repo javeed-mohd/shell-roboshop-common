6f8ed990bf8ed2f120a5157d8a471da6fdc57eea
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-11 15:00:33 +0530

    actions

--- common.sh
+++ common.sh
@@ -64,6 +64,17 @@ java_setup(){
     VALIDATE $? "Moving and Renaming $app_name"
 }
 
+python_setup(){
+
+    dnf install python3 gcc python3-devel -y &>> $LOGS_FILE
+    VALIDATE $? "Installing Python"
+
+    cd /app 
+    pip3 install -r requirements.txt &>> $LOGS_FILE
+    VALIDATE $? "Installing Dependencies"
+
+}
+
 app_setup(){
 
     # 2.Creating the System User
